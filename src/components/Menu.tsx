623504d9c9475d4a25ee765e5a112c993f93a934
geochronology <54387644+geochronology@users.noreply.github.com>
2021-01-07 23:32:33 -0800

    split boilerplate dashboard html into components

---
+++ src/components/Menu.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+
+const Menu = () => (
+
+  <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div className="position-sticky pt-3">
+      <ul className="nav flex-column">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Dashboard
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+)
+
+
+export default Menu
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
